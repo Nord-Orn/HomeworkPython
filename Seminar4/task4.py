df8972d656adcfeb66975bad1461f51cf8f4c20f
«Andrey_Nord» <spglav@mail.ru>
2022-10-10 19:25:38 +0300

    Seminar 4 Task 4

--- Seminar4/task4.py
+++ Seminar4/task4.py
@@ -4,43 +4,27 @@
 # k=2 => 2*x^2 + 4*x + 5 = 0 или x^2 + 5 = 0 или 10*x^2 = 0
 
 from random import randint
- 
-print('Введите натуральную степень k:')
-k = int(input())
 
-
-
- 
-def write_file(st):
-    with open('Task04.txt', 'w') as data:
-        data.write(st)
- 
-def create_list(k):
+def funk(k):
+    file = open ("C:\\Users\\user\\Desktop\\Мое обучение\\Git repositories\\Python\\Seminar4\\file.txt", "w")
     list = []
-    for i in range(k + 1):
-        list.append(randint(0, 101))    
-    return list
-    
-def create_str(sp):
-    list = sp[::-1]
-    wr = ''
-    if len(list) < 1:
-        wr = 'x = 0'
-    else:
-        for i in range(len(list)):
-            if i != len(list) - 1 and list[i] != 0 and i != len(list) - 2:
-                wr += f'{list[i]}x^{len(list) - i - 1}'
-                if list [i + 1] != 0:
-                    wr += ' + '
-            elif i == len(list) - 2 and list[i] != 0:
-                wr += f'{list[i]}x'
-                if list[i + 1] != 0:
-                    wr += ' + '
-            elif i == len(list) - 1 and list[i] != 0:
-                wr += f'{list[i]} = 0'
-            elif i == len(list) - 1 and list[i] == 0:
-                wr += ' = 0'
-    return wr
- 
-koef = create_list(k)
-write_file(create_str(koef))
+    a= int(0)
+    for i in range(-k, -1):
+        a = randint(0, 101)
+        if a != 0:
+            file.write(str(a))
+            file.write("*x^")
+            file.write(str(i))
+            file.write('+')
+
+    a = randint(0, 101)
+    if a != 0:
+        file.write(str(a))
+        file.write("*x+")
+    a = randint(0, 101)
+    if a != 0:
+        file.write(str(a))
+        file.write('=0')
+
+k = int(input('Введите степень k = '))
+funk(k)
